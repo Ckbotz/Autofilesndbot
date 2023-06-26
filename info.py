967ec5728f59b5c8862e8732dbb268fddfaeb37a
Kichuprozz <128233036+Kichuprozz@users.noreply.github.com>
2023-06-26 23:12:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -69,7 +69,6 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
 PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "False")), False)
 PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "False")), True)
-openai.api_key  = environ.get('openai.api_key, ' ')
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
