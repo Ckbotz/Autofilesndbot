b193d9bd02c1cd04b3cb41a613e37b6002c88626
Ckflicks <109861478+Kichuprobotz@users.noreply.github.com>
2024-06-30 12:55:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1547,7 +1547,7 @@ async def advantage_spell_chok(client, msg):
     spell_check_del = await msg.reply_photo(
         photo=(SPELL_IMG),
         caption=(script.CUDNT_FND.format(mv_rqst)),
-        reply_markup=InlineKeyboardMarkup(btn)
+        reply_markup=InlineKeyboardMarkup(btn),quote=True
     )
     try:
         if settings['auto_delete']:
